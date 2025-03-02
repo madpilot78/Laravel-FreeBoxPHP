76431fb5a03b08f23cd46849ea6b9a4c3a3b4e23
Guido Falsi <mad@madpilot.net>
2025-03-02 21:58:21 +0100

    Prefer make method, fetch concrete class

--- src/FreeBoxPHPServiceProvider.php
+++ src/FreeBoxPHPServiceProvider.php
@@ -6,8 +6,8 @@ namespace madpilot78\LaravelFreeBoxPHP;
 
 use madpilot78\LaravelFreeBoxPHP\Commands\RegisterCommand;
 use Illuminate\Contracts\Foundation\Application;
+use Illuminate\Log\LogManager;
 use Illuminate\Support\ServiceProvider;
-use Psr\Log\LoggerInterface;
 use madpilot78\FreeBoxPHP\Box;
 use madpilot78\FreeBoxPHP\Configuration;
 use madpilot78\FreeBoxPHP\Enum\BoxType;
@@ -52,7 +52,7 @@ class FreeBoxPHPServiceProvider extends ServiceProvider
                     https: config('freebox.https', true),
                     localAccess: config('freebox.localaccess', true),
                     boxType: BoxType::tryFrom(config('freebox.boxtype', '')) ?? BoxType::Free,
-                    logger: $app->get(LoggerInterface::class)->channel(config('freebox.log_channel', 'null')),
+                    logger: $app->make(LogManager::class)->channel(config('freebox.log_channel', 'null')),
                     container: $app,
                     timeout: config('freebox.timeout', 30),
                     tokenTTL: config('freebox.tokenttl', 60),
